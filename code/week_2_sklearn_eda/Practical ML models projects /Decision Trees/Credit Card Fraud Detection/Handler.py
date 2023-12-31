c36ecbb21afda3c927a97e60c3210d4aed723487
“LeonDoungala22 <doungala.leon@gmail.com>
2023-12-31 21:50:27 +0100

    Last update

---
+++ code/week_2_sklearn_eda/Practical ML models projects /Decision Trees/Credit Card Fraud Detection/Handler.py
@@ -0,0 +1,177 @@
+import matplotlib.pyplot as plt
+import math
+import mplcursors
+import numpy as np
+import pandas as pd 
+import seaborn as sns
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+from scipy.stats import zscore
+import pickle
+
+
+
+def custum_corr_matrix (df):
+    # Calculate correlation matrix
+    correlation_matrix = df.corr()
+
+    # Set up the matplotlib figure
+    plt.figure(figsize=(20, 12))
+
+    # Plot the heatmap
+    heatmap = sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", fmt=".2f", linewidths=.5, cbar_kws={"shrink": 0.75})
+
+    # Increase font size of annotations
+    heatmap.set_xticklabels(heatmap.get_xticklabels(), fontsize=12)
+    heatmap.set_yticklabels(heatmap.get_yticklabels(), fontsize=12)
+
+    # Rotate y-axis labels for better readability
+    plt.yticks(rotation=0)
+
+    # Add color bar
+    cbar = heatmap.collections[0].colorbar
+    cbar.ax.tick_params(labelsize=12)
+
+    # Add a title
+    plt.title("Correlation Matrix", fontsize=16)
+
+    # Show the plot
+    plt.show()
+
+################################################################
+      
+    
+def handle_outliers(df):
+    """
+    Identify and replace outliers in a DataFrame using the Z-score method.
+
+    Parameters:
+    - df (pd.DataFrame): Input DataFrame containing numerical columns.
+
+    Returns:
+    None
+    """
+
+    z_threshold = 5
+
+    # Display the percentage of outliers before replacement
+    print("# Percentage of Outliers Before Replacement")
+    for column in df.columns:
+        if df[column].dtype in ['int64', 'float64']:
+            z_scores = zscore(df[column])
+            original_outliers_percentage = (abs(z_scores) > z_threshold).sum() / len(df) * 100
+            print(f"- Outliers in {column}: {original_outliers_percentage:.2f}%")
+
+    # Replace outliers with the median
+    for column in df.columns:
+        if df[column].dtype in ['int64', 'float64']:
+            z_scores = zscore(df[column])
+            is_outlier = abs(z_scores) > z_threshold
+            df[column] = np.where(is_outlier, df[column].median(), df[column])
+
+    # Display the percentage of replaced outliers after replacement
+    print("\n# Percentage of Replaced Outliers After Replacement")
+    for column in df.columns:
+        if df[column].dtype in ['int64', 'float64']:
+            replaced_outliers_percentage = (df[column] != df[column].median()).sum() / len(df) * 100
+            print(f"- Outliers replaced in {column}: {replaced_outliers_percentage:.2f}%")
+
+    # Display the percentage of outliers after replacement
+    print("\n# New Percentage of Outliers After Replacement")
+    for column in df.columns:
+        if df[column].dtype in ['int64', 'float64']:
+            new_outliers_percentage = (abs(zscore(df[column])) > z_threshold).sum() / len(df) * 100
+            print(f"- Outliers in {column} after replacement: {new_outliers_percentage:.2f}%")
+
+                
+################################################################
+                
+                
+def evaluate_and_plot_performance(y_test, y_pred, class_labels=None):
+    """
+    Evaluate the performance of a classification model and plot the confusion matrix.
+
+    Parameters:
+    - y_test: True labels of the test set
+    - y_pred: Predicted labels of the test set
+    - class_labels: Labels for classes (default=None, assumes binary classification)
+
+    Returns:
+    - None
+    """
+    # Performance metrics
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+
+    # Display metrics
+    print("Performance metrics for model:\n")
+    print(f"Accuracy: {accuracy * 100:.2f}%")
+    print(f"Precision: {precision * 100:.2f}%")
+    print(f"Recall: {recall * 100:.2f}%")
+    print(f"F1 Score: {f1 * 100:.2f}%")
+
+    # Display confusion matrix
+    print("\nConfusion Matrix:")
+    cm = confusion_matrix(y_test, y_pred)
+
+    # Plot confusion matrix
+    sns.heatmap(cm, annot=True, fmt="d", cmap="YlGnBu", cbar=False,
+                xticklabels=class_labels, yticklabels=class_labels)
+    plt.title('Confusion Matrix')
+    plt.xlabel('Predicted')
+    plt.ylabel('Actual')
+    plt.show()
+
+    # Additional metrics
+    print("\nAdditional Metrics:")
+    print(f'Accuracy: {accuracy * 100:.2f}%')
+
+# Example usage
+# evaluate_and_plot_performance(y_test, y_pred_rbf_tuned, class_labels=np.unique(y))
+
+################################################################
+      
+
+def save_dataset(data, file_path):
+    with open(file_path, 'wb') as data_file:
+        pickle.dump(data, data_file)
+        print(f"Data saved successfully: {file_path}")
+        
+        
+########################################
+
+def balance_classes(X_train, y_train, desired_ratio=1/3, random_state=42):
+    # Combine features and labels into a single DataFrame
+    df = pd.concat([X_train, y_train], axis=1)
+
+    # Separate majority and minority classes
+    df_majority = df[df['Class'] == 0]
+    df_minority = df[df['Class'] == 1]
+
+    # Determine the number of instances needed for the minority class
+    target_minority_size = int(len(df_majority) * (2 * desired_ratio))  # 2/3 of class 1
+
+    # Check if the minority class has at least one instance
+    if not df_minority.empty:
+        # Check if the target minority size is greater than zero
+        if target_minority_size > 0:
+            # Oversample the minority class
+            df_minority_oversampled = df_minority.sample(n=target_minority_size, replace=True, random_state=random_state)
+
+            # Concatenate the oversampled minority class with the majority class
+            df_balanced = pd.concat([df_majority, df_minority_oversampled])
+
+            # Shuffle the DataFrame to mix the classes
+            df_balanced = df_balanced.sample(frac=1, random_state=random_state).reset_index(drop=True)
+
+            # Separate features and labels
+            X_balanced = df_balanced.drop('Class', axis=1)
+            y_balanced = df_balanced['Class']
+
+            return X_balanced, y_balanced
+    else:
+        # If the minority class has no instances, return the original data
+        return X_train, y_train
+
+
---
+++ code/week_2_sklearn_eda/Practical ML models projects /Decision Trees/Credit Card Fraud Detection/assignment.md
@@ -0,0 +1,50 @@
+# Decision Tree Classification Project : Credit Card Fraud Detection
+
+## Project Overview
+- **Objective:** Develop a decision tree classifier for fraudulent credit card transactions.
+- **Dataset:** [Credit Card Fraud Detection](https://www.kaggle.com/mlg-ulb/creditcardfraud)
+
+## Data Importation and Statistics
+- **Data Source:** Specify and import the credit card fraud dataset.
+- **Initial Exploration:** Highlight key features and provide brief statistics.
+- **Class Distribution:** Check for balance between fraudulent and non-fraudulent transactions.
+- **Data Types and Missing Values:** Handle data types, missing values, and duplicates.
+
+## Exploring and Visualizing Data
+- **Transaction Analysis:** Examine the distribution of transaction amounts.
+- **Feature Distributions:** Compare features between genuine and fraudulent transactions.
+- **Correlation Analysis:** Visualize feature relationships for fraud detection.
+
+## Data Preprocessing
+- **Outlier Handling:** Manage outlier transaction amounts.
+- **Feature Scaling:** Normalize feature sizes for decision tree optimization.
+- **Handling Missing Values:** Fill in missing data and assess its impact.
+- **Data Encoding:** Convert words into numerical values for model input.
+
+## Model Building and Evaluation
+- **Algorithm Selection:** Justify the choice of the decision tree algorithm.
+- **Feature Importance Analysis:** Explain the method for determining feature importance.
+- **Cross-Validation Strategies:** Employ cross-validation for robust model evaluation.
+- **Evaluation Metrics:** Define metrics used to assess model performance.
+- **Model Comparison:** Compare decision tree performance with other algorithms.
+- **Validation Set Analysis:** Discuss the use of a validation set.
+- **Hyperparameter Tuning:** Optimize decision tree performance through fine-tuning.
+- **Ensemble Methods Comparison:** Evaluate the effectiveness of Random Forest.
+- **Performance Trade-offs:** Address interpretability vs. accuracy trade-offs.
+- **Confusion Matrix Analysis:** Analyze the confusion matrix for insights.
+
+## Deep Understanding and Visualization
+- **Key Decision Nodes Analysis:** Identify critical decision nodes for fraud detection.
+- **Decision Boundary Visualization:** Illustrate how the decision tree classifies transactions.
+- **Partial Dependence Plots:** Visualize relationships between features and fraud.
+
+## Model Evaluation and Conclusion
+- **Model Performance:** Assess decision tree effectiveness using relevant metrics.
+- **Interpretability vs. Accuracy Balance:** Discuss the trade-off between model interpretability and accuracy.
+- **Feature Importance Insights:** Communicate crucial aspects for fraud detection.
+
+## Conclusion and Future Work
+- **Recommendations for Improvement:** Propose ideas for enhancing the decision tree.
+- **Business Impact:** Explore real-world applications and benefits.
+- **Ethical Considerations:** Reflect on ethical considerations and measures taken.
+- **Future Work:** Outline future avenues for the decision tree model.
